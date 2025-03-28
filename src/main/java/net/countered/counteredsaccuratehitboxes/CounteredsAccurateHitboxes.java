1d2470d951d8f370b35dede1fe2f3f7003f563e2
Bondel Erik <eb102@hdm-stuttgart.de>
2025-03-28 22:56:47 +0100

    Initial commit

---
+++ src/main/java/net/countered/counteredsaccuratehitboxes/CounteredsAccurateHitboxes.java
@@ -0,0 +1,17 @@
+package net.countered.counteredsaccuratehitboxes;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CounteredsAccurateHitboxes implements ModInitializer {
+	public static final String MOD_ID = "countereds-accurate-hitboxes";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+				LOGGER.info("Initializing Countered's Accurate Hitboxes");
+	}
+}
